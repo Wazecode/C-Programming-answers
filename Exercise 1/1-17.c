f2336c2b9253446c2a3fde04e7b033ad2bac5f95
Shuwais <shuwais2003@protonmail.com>
2022-06-26 19:42:34 +0530

    1-17 submitted

---
+++ Exercise 1/1-17.c
@@ -0,0 +1,24 @@
+/*Write a program to print lines greater than 80 char */
+#include <stdio.h>
+#define ARRAY_SIZE 1000
+
+void reset();
+
+int main(){
+  int c;
+  int len = 0;
+  char line[ARRAY_SIZE];
+
+  while ((c = getchar()) != EOF) {
+    if (len > ARRAY_SIZE-2) {
+      printf("ERROR characters exceded 1000 letters");
+    }
+
+    line[len++] = c;
+    if (line[len-1] == '\n') {
+      line[len] = '\0';
+      if (len > 79) printf("%s",line);
+      len = 0;
+    }
+  }
+}
