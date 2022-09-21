757a731c3789bf6d285f306ec0f4b2eef28d12c1
Pica <shuwais2003@protonmail.com>
2022-09-21 11:27:13 +0530

    2-8 completed

--- Exercise 2/2-8.c
+++ Exercise 2/2-8.c
@@ -2,21 +2,40 @@
 #include <math.h>
 
 unsigned shift_pos(unsigned x, int n, int new_n) {
-    unsigned p = pow(2,n);
-    return (~(p & x) << (new_n - n)) & x;
+    unsigned p = pow(2,n-1);
+    if(n < new_n)
+        return ((p & x) << (new_n - n));
+    else
+        return ((p & x) >> (n - new_n));
+        
+}
+
+unsigned rot(unsigned x, int l) {
+        int i = 0;
+        unsigned rot_x = 0;
+        for(i = 0; i <= l; i++) 
+                rot_x = rot_x | shift_pos(x, i, l-i+1);
+        return rot_x;
 }
 
 unsigned rightrot(unsigned x, int n) {
     unsigned mask = (~0 << n);
     unsigned part = x & ~mask;
 
-    return 0;
+    unsigned rot_part = rot(part, n);
+
+    return (x&mask)|rot_part;
 }
 
 int main() {
 
-    unsigned x = 16;
-    printf("%u\n",shift_pos(x,5,6));
+    unsigned x = 13;
+    int n;
+    scanf("%u%d",&x,&n);
+    printf("%u\n",x >> n);
+    printf("%u\n",shift_pos(x, 4, n));
+    printf("%u\n",rot(x,n));
+    printf("%u\n",rightrot(x, n));
 
     return 0;
 }
