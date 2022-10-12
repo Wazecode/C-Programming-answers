b6508f36a88c609d79773b463f2d254bfc1471af
Pica <shuwais2003@protonmail.com>
2022-10-12 08:34:45 +0530

    3-1 completed

--- Exercise 3/3-1.c
+++ Exercise 3/3-1.c
@@ -2,31 +2,27 @@
 
 int binsearch(int x, int v[], int n) {
     int low = 0;
-    int high = n - 1;
-    int mid;
-    while(low < high) {
+    int high = n - 1; int mid;
+    while(low != high) {
         mid = (low+high)/2;
-        printf("%d\n",mid);
-        if(x < v[mid])
-            high = mid - 1;
-        else
+        if(x > v[mid])
             low = mid + 1;
+        else
+            high = mid;
+
     }
-    printf("%d\n", low);
     mid = (low+high)/2;
-    if(x == v[mid])
-        return mid;
-    else
-        return -1;
+    if(x == v[low])
+        return low;
+    return -1;
 }
 
 int main() {
     int v[] = {20,30,40,50,100,200,400,800,1000};
-    int x = 20;
+    int x = 100;
 
     int n = sizeof(v)/sizeof(v[0]);
 
     printf("%d", binsearch(x,v,n));
     return 0;
 }
-
