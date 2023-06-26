a7efffc2327ccd5e28be89ec862bfe50f1226031
Shuwais <shuwais2003@protonmail.com>
2023-06-26 12:59:30 +0530

    4-4 completed

---
+++ Exercise 4/4-4.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
+
+#define MAXVAL 100
+#define NUMBER '0'
+
+int getop(char []);
+void push(double);
+double pop();
+void clear_stack();
+
+
+int main() {
+    int type;
+    int itr;
+    double op1, op2;
+    char s[MAXVAL];
+
+    while((type = getop(s)) != EOF) {
+        switch(type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if(op2 != 0)
+                    push(pop() / op2);
+                else
+                    printf("Error: zero divisor\n");
+                break;
+            case '%':
+                op2 = pop();
+                if(op2 != 0)
+                    push(fmod(op2, pop()));
+                else
+                    printf("Error: zero divisor\n");
+                break;
+            case 't': // Top
+                push(op2 = pop());
+                printf("the top element : %g\n", op2);
+                break;
+            case 'd': // duplicate
+                op2 = pop();
+                push(op2);
+                push(op2);
+                break;
+            case 's': // swap
+                op1 = pop();
+                op2 = pop();
+                push(op1);
+                push(op2);
+                break;
+            case 'c': // clear
+                clear_stack();
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+    return 0;
+}
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full can't push %g\n", f);
+}
+
+double pop() {
+    if (sp > 0) 
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+void clear_stack() {
+    sp = 0;
+}
+
+int getch();
+void ungetch(int);
+
+int getop(char s[]) {
+
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t') ;
+
+    s[1] = '\0';
+    if(!isdigit(c) && c != '.') 
+        return c;
+
+    i = 0;
+    if(isdigit(c))
+        while (isdigit(s[++i] = c = getch())) ;
+
+    if(c == '.')
+        while (isdigit(s[++i] = c = getch())) ;
+
+    s[i] = '\0';
+    if( c != EOF)
+        ungetch(c);
+
+    return NUMBER;
+}
+
+#define BUFFSIZE 100
+
+char buf[BUFFSIZE];
+char bufp = 0;
+
+int getch() {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if(bufp >= BUFFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
